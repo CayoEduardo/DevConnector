1d7b9dd022bce87cf0fda6af032cdcc2f1ffc8a2
Cayo Eduardo <cayo.eduardo.silveira@gmail.com>
2020-08-01 19:06:18 -0300

    Backend finished and working as expected.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const User = require('../../models/User')
 const bcrypt = require('bcryptjs')
 
@@ -50,8 +52,21 @@ router.post(
       await user.save()
 
       //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
 
-      res.send('User Registered!')
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (exception) {
       console.error(err.message)
       res.status(500).send('Server error')
